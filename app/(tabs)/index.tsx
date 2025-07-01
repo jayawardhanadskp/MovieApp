169aacb2d2e5ed70208185f8bb543a05f2f74330
Kasun Jayawardhana <kasunpradeep.ds@gmail.com>
2025-07-02 03:13:08 +0530

    `feat: updated Index component with new imports, useFetch hook, and FlatList`

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,25 +1,59 @@
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
-import { Image, ScrollView, Text, View } from "react-native";
-import SearchBar from "@/components/SearchBar"; 
+import { ActivityIndicator, FlatList, Image, ScrollView, Text, View } from "react-native";
+import SearchBar from "@/components/SearchBar";
 import { useRouter } from "expo-router";
+import useFetch from "@/services/useFetch";
+import { fetchMovies } from "@/services/api";
 
 export default function Index() {
   const router = useRouter();
 
+  const { data: movies, loading: movieLoading, error: moviesError } = useFetch(() => fetchMovies({
+    query: ''
+  }))
+
   return (
     <View className="flex-1 bg-black">
-      <Image source={images.bg} className="absolute w-full z-0"/> 
+      <Image source={images.bg} className="absolute w-full z-0" />
 
-      <ScrollView className="flex-1 px-5" showsVerticalScrollIndicator={false} contentContainerStyle={{minHeight: "100%", paddingBottom: 10}}>
-        <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto"/>
+      <ScrollView className="flex-1 px-5" showsVerticalScrollIndicator={false} contentContainerStyle={{ minHeight: "100%", paddingBottom: 10 }}>
+        <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
 
-        <View className="flex-1 mt-5">
-          <SearchBar 
-            onPress = {() => router.push("/search")}
-            placeholder = "Search for a movie"
+        {movieLoading ? (
+          <ActivityIndicator
+            size="large"
+            color="#0000ff"
+            className="mt-10 self-center"
           />
-        </View>
+        ) : moviesError ? (
+          <Text>Error: {moviesError?.message}</Text>
+        ) : (
+          <View className="flex-1 mt-5">
+            <SearchBar
+              onPress={() => router.push("/search")}
+              placeholder="Search for a movie"
+            />
+
+            <>
+              <Text className="text-lg text-white font-bold mr-5 mb-3">
+                Latest Movies
+              </Text>
+
+              <FlatList
+                data={movies}
+                keyExtractor={(item) => item.id.toString()} // assuming item.id exists
+                renderItem={({ item }) => (
+                  <Text className="text-white text-sm mb-3">{item.title}</Text>
+                )}
+                contentContainerStyle={{ paddingBottom: 40 }}
+                showsVerticalScrollIndicator={false}
+              />
+            </>
+          </View>
+        )}
+
+
       </ScrollView>
     </View>
   );
